f518d1e9113117c309db1f1f78888dd69feb8108
Rimpy Bala <120065454+rimpybala2089@users.noreply.github.com>
2023-03-13 16:43:53 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "dev-tf-state-bucket-dev"
     key = "main"
-    region = "us-east-1"
+    region = "us-west-2"
     dynamodb_table = "tf-dynamodb-table"
   }
 }
